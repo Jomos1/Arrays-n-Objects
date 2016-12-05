0f72512eddf808d477f7dd6febdb921e55e2dddf
Student <idk123@gmail.com>
2016-12-05 08:39:12 -1000

    modified:   hi.js

---
+++ hi.js
@@ -0,0 +1,22 @@
+/*
+
+	Jomari Jacinto
+	Arrays & Objects
+	6 Dec. 2016
+
+*/
+
+var numbers = [5,55,69,96,0,1,8,59,2,33];
+var names = ["Skrub Tan", "Zakattack", "Yoloha", "Ohaiyo", "Hugh Mungus"];
+var colors = ["light black", "dark black", "black", "blue team", "red team"];
+var adj = ["smurt", "special case", "yummy pony", "tiny", "floof"];
+var  pidgin = ["scrap", "beef", "da kine", "ly 'dat", "pau hana"];
+
+//OBJECTS LOLOLOLOLOL
+var donut = {
+	toppings : ["sprinkles", "frosting", "chocolate", "jalapeno"],
+	fillings : "centipedes",
+	shape : "squares",
+	count : 12
+
+}
